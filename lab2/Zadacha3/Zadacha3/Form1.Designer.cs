8eb0ca41bae99ce54c04a00255d32ccd87498395
Mario Kolev <kolev.mario98@gmail.com>
2019-10-06 16:25:07 +0300

    Measure units converter/Zadacha 3

---
+++ lab2/Zadacha3/Zadacha3/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace Zadacha3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.inputBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.btn_calc = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.resultBox = new System.Windows.Forms.TextBox();
+            this.btn_close = new System.Windows.Forms.Button();
+            this.btn_refresh = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Convert:";
+            // 
+            // inputBox
+            // 
+            this.inputBox.Location = new System.Drawing.Point(15, 43);
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(100, 20);
+            this.inputBox.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(156, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "From";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "feet",
+            "yards",
+            "inches",
+            "miles"});
+            this.comboBox1.Location = new System.Drawing.Point(156, 42);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(156, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "To";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "feet",
+            "yards",
+            "inches",
+            "miles"});
+            this.comboBox2.Location = new System.Drawing.Point(156, 99);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 5;
+            // 
+            // btn_calc
+            // 
+            this.btn_calc.Location = new System.Drawing.Point(121, 144);
+            this.btn_calc.Name = "btn_calc";
+            this.btn_calc.Size = new System.Drawing.Size(75, 23);
+            this.btn_calc.TabIndex = 6;
+            this.btn_calc.Text = "Calculate";
+            this.btn_calc.UseVisualStyleBackColor = true;
+            this.btn_calc.Click += new System.EventHandler(this.btn_calc_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 130);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Result:";
+            // 
+            // resultBox
+            // 
+            this.resultBox.Location = new System.Drawing.Point(15, 146);
+            this.resultBox.Name = "resultBox";
+            this.resultBox.Size = new System.Drawing.Size(100, 20);
+            this.resultBox.TabIndex = 8;
+            // 
+            // btn_close
+            // 
+            this.btn_close.Location = new System.Drawing.Point(15, 191);
+            this.btn_close.Name = "btn_close";
+            this.btn_close.Size = new System.Drawing.Size(75, 23);
+            this.btn_close.TabIndex = 9;
+            this.btn_close.Text = "Close";
+            this.btn_close.UseVisualStyleBackColor = true;
+            this.btn_close.Click += new System.EventHandler(this.btn_close_Click);
+            // 
+            // btn_refresh
+            // 
+            this.btn_refresh.Location = new System.Drawing.Point(96, 191);
+            this.btn_refresh.Name = "btn_refresh";
+            this.btn_refresh.Size = new System.Drawing.Size(75, 23);
+            this.btn_refresh.TabIndex = 10;
+            this.btn_refresh.Text = "Refresh";
+            this.btn_refresh.UseVisualStyleBackColor = true;
+            this.btn_refresh.Click += new System.EventHandler(this.btn_refresh_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(296, 240);
+            this.Controls.Add(this.btn_refresh);
+            this.Controls.Add(this.btn_close);
+            this.Controls.Add(this.resultBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btn_calc);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.inputBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Measure units converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox inputBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button btn_calc;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox resultBox;
+        private System.Windows.Forms.Button btn_close;
+        private System.Windows.Forms.Button btn_refresh;
+    }
+}
+
---
+++ lab2/Zadacha3/Zadacha3/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zadacha3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //function for convert 
+        public double m_unitConvert(string from_unit, string to_unit, double x){
+            double result = 0;
+            if(from_unit == "feet" && to_unit == "yards")
+            {
+               result = x / 3;
+            }else if(from_unit == "feet" && to_unit == "inches")
+            {
+                result = x * 12;
+            }else if(from_unit == "feet" && to_unit == "miles")
+            {
+                result = x / 5280.0;
+            }else if(from_unit == "yards" && to_unit == "feet")
+            {
+                result = x * 3;
+            }
+            else if (from_unit == "yards" && to_unit == "inches")
+            {
+                result = x * 36;
+            }
+            else if (from_unit == "yards" && to_unit == "miles")
+            {
+                result = x * 0.000568; 
+            }
+            else if (from_unit == "inches" && to_unit == "feet")
+            {
+                result = x * 0.0833333333; ;
+            }
+            else if (from_unit == "inches" && to_unit == "yards")
+            {
+                result = x/ 36;
+            }
+            else if (from_unit == "inches" && to_unit == "miles")
+            {
+                result = x / 63360;
+            }
+            else if (from_unit == "miles" && to_unit == "feet")
+            {
+                result = x* 5280.0;
+            }
+            else if (from_unit == "miles" && to_unit == "yards")
+            {
+                result = x * 1760;
+            }
+            else if (from_unit == "miles" && to_unit == "inches")
+            {
+                result = x * 63360;
+            }
+
+            return result;
+
+            }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btn_calc_Click(object sender, EventArgs e)
+        {
+            double a = Double.Parse(inputBox.Text);
+            resultBox.Text = m_unitConvert(comboBox1.SelectedItem.ToString(), comboBox2.SelectedItem.ToString(), a).ToString();
+
+        }
+
+        private void btn_refresh_Click(object sender, EventArgs e)
+        {
+            inputBox.Clear();
+            resultBox.Clear();
+            comboBox1.SelectedIndex = -1;
+            comboBox2.SelectedIndex = -1;
+        }
+
+        private void btn_close_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
