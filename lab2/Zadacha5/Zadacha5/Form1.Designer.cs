439a51d75d0be72cb6591515ba0c8f817b70221d
Mario Kolev <kolev.mario98@gmail.com>
2019-10-07 13:47:14 +0300

    El kalkulator

---
+++ lab2/Zadacha5/Zadacha5/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace Zadacha5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.менюToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.новКлиентToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.заПрограматаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.изходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.client_n = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pot_lbl = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.label7 = new System.Windows.Forms.Label();
+            this.suma_lbl = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.менюToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(515, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // менюToolStripMenuItem
+            // 
+            this.менюToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.новКлиентToolStripMenuItem,
+            this.заПрограматаToolStripMenuItem,
+            this.изходToolStripMenuItem});
+            this.менюToolStripMenuItem.Name = "менюToolStripMenuItem";
+            this.менюToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.менюToolStripMenuItem.Text = "Меню";
+            // 
+            // новКлиентToolStripMenuItem
+            // 
+            this.новКлиентToolStripMenuItem.Name = "новКлиентToolStripMenuItem";
+            this.новКлиентToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.новКлиентToolStripMenuItem.Text = "Нов клиент";
+            this.новКлиентToolStripMenuItem.Click += new System.EventHandler(this.новКлиентToolStripMenuItem_Click);
+            // 
+            // заПрограматаToolStripMenuItem
+            // 
+            this.заПрограматаToolStripMenuItem.Name = "заПрограматаToolStripMenuItem";
+            this.заПрограматаToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.заПрограматаToolStripMenuItem.Text = "За програмата";
+            this.заПрограматаToolStripMenuItem.Click += new System.EventHandler(this.заПрограматаToolStripMenuItem_Click);
+            // 
+            // изходToolStripMenuItem
+            // 
+            this.изходToolStripMenuItem.Name = "изходToolStripMenuItem";
+            this.изходToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.изходToolStripMenuItem.Text = "Изход";
+            this.изходToolStripMenuItem.Click += new System.EventHandler(this.изходToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(160, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Клиент номер :";
+            // 
+            // client_n
+            // 
+            this.client_n.AutoSize = true;
+            this.client_n.Location = new System.Drawing.Point(251, 58);
+            this.client_n.Name = "client_n";
+            this.client_n.Size = new System.Drawing.Size(13, 13);
+            this.client_n.TabIndex = 2;
+            this.client_n.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(116, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(179, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Старо показание на електромера";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(116, 167);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(175, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Ново показание на електромера";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(314, 108);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(314, 167);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(116, 213);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Потребление:";
+            // 
+            // pot_lbl
+            // 
+            this.pot_lbl.AutoSize = true;
+            this.pot_lbl.Location = new System.Drawing.Point(257, 213);
+            this.pot_lbl.Name = "pot_lbl";
+            this.pot_lbl.Size = new System.Drawing.Size(13, 13);
+            this.pot_lbl.TabIndex = 8;
+            this.pot_lbl.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(116, 248);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(46, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Тарифа";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(116, 278);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(46, 17);
+            this.radioButton1.TabIndex = 10;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "0.34";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(192, 278);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(46, 17);
+            this.radioButton2.TabIndex = 11;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "0.44";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(257, 278);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(46, 17);
+            this.radioButton3.TabIndex = 12;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "0.54";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(119, 327);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(102, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Сума за плащане :";
+            // 
+            // suma_lbl
+            // 
+            this.suma_lbl.AutoSize = true;
+            this.suma_lbl.Location = new System.Drawing.Point(254, 327);
+            this.suma_lbl.Name = "suma_lbl";
+            this.suma_lbl.Size = new System.Drawing.Size(13, 13);
+            this.suma_lbl.TabIndex = 14;
+            this.suma_lbl.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(515, 377);
+            this.Controls.Add(this.suma_lbl);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.pot_lbl);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.client_n);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "El. calculator";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem менюToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem новКлиентToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem заПрограматаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem изходToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label client_n;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label pot_lbl;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label suma_lbl;
+    }
+}
+
---
+++ lab2/Zadacha5/Zadacha5/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Zadacha5
+{
+    
+    public partial class Form1 : Form
+    {
+       
+        List<double> pot_list = new List<double>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public double Calc(double x1, double x2, double tarifa)
+        {
+            double res = 0;
+            res = (x2 - x1) * tarifa;
+            return res;
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            try { 
+            suma_lbl.Text = Calc(Double.Parse(textBox1.Text), Double.Parse(textBox2.Text), 0.34).ToString();
+            pot_lbl.Text = (Double.Parse(textBox2.Text) - Double.Parse(textBox1.Text)).ToString();
+          
+                
+            }
+            catch { }
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                suma_lbl.Text = Calc(Double.Parse(textBox1.Text), Double.Parse(textBox2.Text), 0.44).ToString();
+                pot_lbl.Text = (Double.Parse(textBox2.Text) - Double.Parse(textBox1.Text)).ToString();
+               
+            }
+            catch { }
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                suma_lbl.Text = Calc(Double.Parse(textBox1.Text), Double.Parse(textBox2.Text), 0.54).ToString();
+                pot_lbl.Text = (Double.Parse(textBox2.Text) - Double.Parse(textBox1.Text)).ToString();
+              
+
+            }
+            catch { }
+
+        }
+
+        private void новКлиентToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            int num = Int32.Parse(client_n.Text);
+            num++;
+            client_n.Text = num.ToString();
+            pot_list.Add(Double.Parse(suma_lbl.Text));
+            textBox1.Clear();
+            textBox2.Clear();
+            pot_lbl.Text = "0";
+            suma_lbl.Text = "0";
+            radioButton1.Checked = false;
+            radioButton2.Checked = false;
+            radioButton3.Checked = false;
+
+        }
+
+        private void изходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            pot_list.Add(Double.Parse(suma_lbl.Text));
+            double x = 0;
+            for(int i = 0; i < pot_list.Count; i++)
+            {
+                 x = x + pot_list[i];
+            }
+            DialogResult dialog = MessageBox.Show("Общо за всички = " + x + " лв");
+            if(dialog == DialogResult.OK)
+            {
+                 Application.Exit();
+            }
+
+        }
+
+        private void заПрограматаToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Програма, която изчислява потреблението на ток.", "About");
+        }
+    }
+}
