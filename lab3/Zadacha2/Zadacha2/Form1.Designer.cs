fa25996a2c2964eddce895ef568c71209c053905
Mario Kolev <kolev.mario98@gmail.com>
2019-10-14 21:07:39 +0300

    Zadacha 2

---
+++ lab3/Zadacha2/Zadacha2/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace Zadacha2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.text_salary = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btn_calc = new System.Windows.Forms.Button();
+            this.label_doo = new System.Windows.Forms.Label();
+            this.label_dzpo = new System.Windows.Forms.Label();
+            this.label_zdravno = new System.Windows.Forms.Label();
+            this.label_sum = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.Location = new System.Drawing.Point(131, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(284, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Калкулатор за дължими осигуровки";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(45, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Заплата";
+            // 
+            // text_salary
+            // 
+            this.text_salary.Location = new System.Drawing.Point(100, 101);
+            this.text_salary.Name = "text_salary";
+            this.text_salary.Size = new System.Drawing.Size(100, 20);
+            this.text_salary.TabIndex = 2;
+            this.text_salary.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.text_salary_KeyPress);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(312, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "ДОО";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(312, 143);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "ДЗПО";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(45, 154);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(105, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Година на раждане";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(100, 181);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(122, 17);
+            this.radioButton1.TabIndex = 6;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Преди 1960 година";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(100, 204);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(107, 17);
+            this.radioButton2.TabIndex = 7;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "След 01.01.1960";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(312, 171);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(50, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Здравно";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(312, 284);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(36, 13);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Общо";
+            // 
+            // btn_calc
+            // 
+            this.btn_calc.Location = new System.Drawing.Point(48, 274);
+            this.btn_calc.Name = "btn_calc";
+            this.btn_calc.Size = new System.Drawing.Size(75, 23);
+            this.btn_calc.TabIndex = 10;
+            this.btn_calc.Text = "Изчисли";
+            this.btn_calc.UseVisualStyleBackColor = true;
+            this.btn_calc.Click += new System.EventHandler(this.btn_calc_Click);
+            // 
+            // label_doo
+            // 
+            this.label_doo.AutoSize = true;
+            this.label_doo.Location = new System.Drawing.Point(387, 109);
+            this.label_doo.Name = "label_doo";
+            this.label_doo.Size = new System.Drawing.Size(0, 13);
+            this.label_doo.TabIndex = 11;
+            // 
+            // label_dzpo
+            // 
+            this.label_dzpo.AutoSize = true;
+            this.label_dzpo.Location = new System.Drawing.Point(387, 143);
+            this.label_dzpo.Name = "label_dzpo";
+            this.label_dzpo.Size = new System.Drawing.Size(0, 13);
+            this.label_dzpo.TabIndex = 12;
+            // 
+            // label_zdravno
+            // 
+            this.label_zdravno.AutoSize = true;
+            this.label_zdravno.Location = new System.Drawing.Point(387, 171);
+            this.label_zdravno.Name = "label_zdravno";
+            this.label_zdravno.Size = new System.Drawing.Size(0, 13);
+            this.label_zdravno.TabIndex = 13;
+            // 
+            // label_sum
+            // 
+            this.label_sum.AutoSize = true;
+            this.label_sum.Location = new System.Drawing.Point(387, 284);
+            this.label_sum.Name = "label_sum";
+            this.label_sum.Size = new System.Drawing.Size(0, 13);
+            this.label_sum.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(465, 317);
+            this.Controls.Add(this.label_sum);
+            this.Controls.Add(this.label_zdravno);
+            this.Controls.Add(this.label_dzpo);
+            this.Controls.Add(this.label_doo);
+            this.Controls.Add(this.btn_calc);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.text_salary);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox text_salary;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btn_calc;
+        private System.Windows.Forms.Label label_doo;
+        private System.Windows.Forms.Label label_dzpo;
+        private System.Windows.Forms.Label label_zdravno;
+        private System.Windows.Forms.Label label_sum;
+    }
+}
+
---
+++ lab3/Zadacha2/Zadacha2/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zadacha2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void text_salary_KeyPress(object sender, KeyPressEventArgs e)
+        {           
+            if (!char.IsDigit(e.KeyChar) && e.KeyChar != (char)8 && e.KeyChar != (char)44)
+            {
+                e.Handled = true; text_salary.Focus();
+            }
+        }
+       
+
+
+        private void btn_calc_Click(object sender, EventArgs e)
+        {
+            
+        
+            double salary = Double.Parse(text_salary.Text);
+            double DOO = 0;
+            double DZPO = 0;
+            double zdravni_osig = 0.032 * salary;
+            double sum = 0;
+
+            if (radioButton1.Checked)
+            {
+                DOO = 0.089 * salary;
+                DZPO = 0 * salary;
+                sum = DOO + DZPO + zdravni_osig;
+            }else if (radioButton2.Checked)
+            {
+                DOO = 0.067 * salary;
+                DZPO = 0.022 * salary;
+                sum = DOO + DZPO + zdravni_osig;
+            }
+            label_dzpo.Text = Math.Round(DZPO, 2).ToString() + " лв " ;
+            label_zdravno.Text = Math.Round(zdravni_osig, 2).ToString() + " лв ";
+            label_doo.Text = Math.Round(DOO, 2).ToString() + " лв ";
+            label_sum.Text = Math.Round(sum, 2).ToString() + " лв ";
+            }
+          
+        }
+
+   
+
+}
+
